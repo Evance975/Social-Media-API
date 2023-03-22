a5f187c222ed514d57a1ffa32192f7940a0440eb
Evance975 <evanceomolo975@gmail.com>
2023-03-22 15:16:05 +0300

    First commit

---
+++ app/schemas.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+#our schema/pydantic model
+    
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int 
+    created_at: datetime
+
+    #converts the SQL response from the database into a valid Dict
+    class Config:
+        orm_mode = True  
+        
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    
+    class Config:
+        orm_mode = True  
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    id: Optional[str] = None
